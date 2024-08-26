be2d635a1de03e5653d837ed050f29d7b580a09e
carisaelam <carisaelam@gmail.com>
2024-08-26 16:47:12 -0400

    Seed Flight data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,36 @@ Airport.create!([
 ])
 
 p "Created #{Airport.count} airports."
+
+
+Flight.destroy_all
+
+Flight.create!([
+  {
+    departure_airport: Airport.first,
+    arrival_airport: Airport.find(2),
+    start_date_time: DateTime.now,
+    flight_duration: 120
+  },
+  {
+    departure_airport: Airport.find(2),
+    arrival_airport: Airport.find(3),
+    start_date_time: DateTime.now,
+    flight_duration: 180
+  },
+  {
+    departure_airport: Airport.find(3),
+    arrival_airport: Airport.find(4),
+    start_date_time: DateTime.now,
+    flight_duration: 300
+  },
+  {
+    departure_airport: Airport.find(2),
+    arrival_airport: Airport.find(1),
+    start_date_time: DateTime.now,
+    flight_duration: 300
+  }
+])
+
+
+p "Created #{Flight.count} flights."
