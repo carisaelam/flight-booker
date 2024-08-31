a2f75585304a75f3fb6eeb152852af260e1b0b86
carisaelam <carisaelam@gmail.com>
2024-08-31 14:58:36 -0400

    Finalize booking confirmation mailer

--- app/javascript/controllers/colorme_controller.js
+++ app/javascript/controllers/colorme_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="colorme"
 export default class extends Controller {
-  static targets = ['text'];
+  static targets = ['text', 'flights', 'search', 'display'];
 
   connect() {
     console.log('colorme active');
@@ -13,7 +13,21 @@ export default class extends Controller {
     this.textTargets.forEach((element) => {
       element.classList.toggle('red');
     });
-    // this.titleTextTarget.classList.toggle('red');
-    // this.otherTitleTextTarget.classList.toggle('red');
+  }
+
+  displayFlights(e) {
+    e.preventDefault();
+    console.log('displayFlights activated');
+    console.log(
+      'Before change, flights target style:',
+      this.flightsTarget.hidden
+    );
+    this.flightsTarget.hidden = false;
+    console.log(
+      'After change, flights target style:',
+      this.flightsTarget.hidden
+    );
+    this.searchTarget.hidden = true;
+    this.displayTarget.hidden = false;
   }
 }
